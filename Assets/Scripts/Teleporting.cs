c0ba0d57f151551ca34cffcd6f248d4198dcf03a
Andrew Yurick <39890868+andrewyurick@users.noreply.github.com>
2022-12-03 12:30:36 -0500

    Teleporting script
    Lets you move the character to another location so that we can teleport to different floors of the learning factory. 
    To use script drag it onto an object. Then for "Teleport Target" set it as another object you want the character to teleport to. 
    For "The Player" set it as PlayerCapsule. 
    Now when you walk into the object, it will teleport your player to the other object. 
    Note: Also go into Edit->Project Settings->Physics-> MAKE SURE Auto Sync Transforms is checked.

---
+++ Assets/Scripts/Teleporting.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporting : MonoBehaviour
+{
+    public Transform teleportTarget;
+    public GameObject thePlayer;
+
+    void OnTriggerEnter(Collider other){
+        thePlayer.transform.position = teleportTarget.transform.position;
+    }
+}
